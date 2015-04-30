e60a63b756c9030aaa9a1c84d4acd568a636715f
Greg <bgreg@users.noreply.github.com>
2015-04-29 21:40:17 -0700

    Create clear_desktop.bash

---
+++ clear_desktop.bash
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+ls ~/Desktop/* &>/dev/null
+if [ $? -eq 0 ]
+then 
+  FILENAME=desktop_backup_$(date | tr " " "_") 
+  zip ~/$FILENAME ~/Desktop/* 1>/dev/null
+  rm $(ls ~/Desktop/*)
+  echo "desktop files were moved to: $(ls ~/$FILENAME.*)"
+fi

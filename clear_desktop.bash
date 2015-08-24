51831dc2e970f7fc54fe657627d02b5890cf6855
Greg <bgreg@users.noreply.github.com>
2015-08-24 13:37:00 -0700

    ignore directories

--- clear_desktop.bash
+++ clear_desktop.bash
@@ -1,10 +1,13 @@
 #! /bin/bash
-# look at quote escaping file names. 
+
 ls ~/Desktop/* &>/dev/null
 if [ $? -eq 0 ]
-then 
-  FILENAME=desktop_backup_$(date | tr " " "_") 
-  zip ~/$FILENAME ~/Desktop/* 1>/dev/null
-  rm $(ls ~/Desktop/*)
-  echo "desktop files were moved to: $(ls ~/$FILENAME.*)"
+then
+  FILENAME="desktop_backup_$(date | tr " " "_")"
+  TMP="$HOME/Desktop/tmp_$(date | tr " " "_")"
+
+  mkdir $TMP
+  find "$HOME/Desktop" -maxdepth 1 -type f -exec mv {} $TMP \;
+  mv $TMP "$HOME/Documents/$FILENAME"
+  echo "desktop files were moved to: $HOME/Desktop/$FILENAME"
 fi

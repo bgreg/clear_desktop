013c03d4c042067db6be29dd6e686abce594f596
Greg <bgreg@users.noreply.github.com>
2015-08-24 20:40:28 -0700

    Handle edge case
    Allow for skipping hidden files and use more idomatic if statement

--- clear_desktop.bash
+++ clear_desktop.bash
@@ -1,13 +1,19 @@
 #! /bin/bash
 
-ls ~/Desktop/* &>/dev/null
-if [ $? -eq 0 ]
-then
-  FILENAME="desktop_backup_$(date | tr " " "_")"
-  TMP="$HOME/Desktop/tmp_$(date | tr " " "_")"
+#! /bin/bash
 
+if find "$HOME/Desktop" -maxdepth 1 -type f | grep -v '/\.' &>/dev/null
+then
+  DATE=$(date | tr " " "_")
+  FILENAME="desktop_backup_$DATE"
+  TMP="$HOME/Desktop/tmp_$DATE"
+  NEW_PATH="$HOME/Documents/$FILENAME"
+
+  # Move desktop files that are not hidden into a tmp folder
   mkdir $TMP
-  find "$HOME/Desktop" -maxdepth 1 -type f -exec mv {} $TMP \;
-  mv $TMP "$HOME/Documents/$FILENAME"
-  echo "desktop files were moved to: $HOME/Desktop/$FILENAME"
+  find "$HOME/Desktop" -not -path "$HOME/Desktop/.*" -maxdepth 1 -type f -exec mv {} $TMP \;
+
+  # move tmp folder to documents and rename it
+  mv $TMP $NEW_PATH 
+  echo "Desktop files were moved to: $NEW_PATH"
 fi
